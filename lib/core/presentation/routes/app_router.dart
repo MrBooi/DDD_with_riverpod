3deb2e7d7a46d860c4bf38bfb4dcde26ebbb9196
Ayabonga <ayabongabooi2@gmail.com>
2022-09-19 21:11:39 +0200

    feat(app_router): register app router

--- lib/core/presentation/routes/app_router.dart
+++ lib/core/presentation/routes/app_router.dart
@@ -1,9 +1,11 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:ddd_riverpod/auth/presentation/sign_in_page.dart';
 import 'package:ddd_riverpod/splash/presentation/splash_page.dart';
 
 @MaterialAutoRouter(
   routes: [
     MaterialRoute(page: SplashPage, initial: true),
+    MaterialRoute(page: SignInPage)
   ],
   replaceInRouteName: 'Page,Route',
 )
