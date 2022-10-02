df65b44fbacd0d3f790d02cd9c584ee6311678a5
Ayabonga <ayabongabooi2@gmail.com>
2022-10-02 19:40:16 +0200

    feat(i_auth_facade): renamed file name

--- lib/auth/domain/i_auth_facade.dart
+++ lib/auth/domain/i_auth_facade.dart
@@ -4,6 +4,5 @@ import 'package:ddd_riverpod/auth/domain/auth_failure.dart';
 abstract class IAuthFacade {
   Future<Either<AuthFailure, Unit>> signInWithGoogle();
   Future<Either<AuthFailure, Unit>> signOut();
-  Future<Option<bool>> isUserExist();
   Future<bool> isSignIn();
 }
