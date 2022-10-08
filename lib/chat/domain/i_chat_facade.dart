1fb2c54e08b2dbc80c4c7ba6f83c406dff0ee319
Ayabonga <ayabongabooi2@gmail.com>
2022-10-08 20:42:58 +0200

    fix(chat_domain): added chat interface class

---
+++ lib/chat/domain/i_chat_facade.dart
@@ -0,0 +1,10 @@
+import 'package:ddd_riverpod/chat/domain/chat_channel_entity.dart';
+
+abstract class IChatFacade {
+  Stream<List<ChatChannelEntity>> chatChannelEntityStream(String uuid);
+  Future<void> updateChatChannel(
+    String channelId,
+    Map<String, dynamic> payload,
+  );
+  Future<ChatChannelEntity?> getChatChannel(String channelId);
+}
