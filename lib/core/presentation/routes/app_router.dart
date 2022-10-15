2bab0f787dbeddfbb533b85f98390b2259403e61
Ayabonga <ayabongabooi2@gmail.com>
2022-10-15 21:39:58 +0200

    feat(app_router): register home page into the app router

--- lib/core/presentation/routes/app_router.dart
+++ lib/core/presentation/routes/app_router.dart
@@ -1,11 +1,13 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:ddd_riverpod/features/auth/presentation/sign_in_page.dart';
+import 'package:ddd_riverpod/features/home/presentation/home_page.dart';
 import 'package:ddd_riverpod/features/splash/presentation/splash_page.dart';
 
 @MaterialAutoRouter(
   routes: [
     MaterialRoute(page: SplashPage, initial: true),
-    MaterialRoute(page: SignInPage)
+    MaterialRoute(page: SignInPage),
+    MaterialRoute(page: HomePage)
   ],
   replaceInRouteName: 'Page,Route',
 )
