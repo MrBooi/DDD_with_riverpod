08f115e0e15bdc35ec76e9523464f9c5346c3efd
Ayabonga <ayabongabooi2@gmail.com>
2022-10-23 21:11:03 +0200

    refactor(provider): update provider access

--- lib/features/chat/shared/provider.dart
+++ lib/features/chat/shared/provider.dart
@@ -4,11 +4,15 @@ import 'package:ddd_riverpod/features/chat/domain/chat_channel_entity.dart';
 import 'package:ddd_riverpod/features/chat/infastracture/chat_facade.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+final chatFacadeProvider = Provider<ChatFacade>((ref) {
+  return ChatFacade(ref.watch(fireStoreProvider));
+});
+
 final chatChannelStreamProvider =
     StreamProvider.autoDispose.family<List<ChatChannelEntity>, String>(
   (ref, userId) {
-    return ChatFacade(ref.watch(fireStoreProvider)).chatChannelEntityStream(
-      userId,
-    );
+    return ref.watch(chatFacadeProvider).chatChannelEntityStream(
+          userId,
+        );
   },
 );
