2bbe931e1b80c1ccb52ec495b4acc476b0c3de66
Ayabonga <ayabongabooi2@gmail.com>
2022-11-09 11:32:14 +0200

    fix(search_item): completed search page

--- lib/features/search/presentation/search_page.dart
+++ lib/features/search/presentation/search_page.dart
@@ -1,6 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:ddd_riverpod/features/search/presentation/widgets/search_app_bar.dart';
+import 'package:ddd_riverpod/features/search/presentation/widgets/search_body.dart';
+import 'package:ddd_riverpod/features/search/shared/provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
@@ -11,14 +14,19 @@ class SearchPage extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            SearchAppBar(
-              onCancel: () {
-                context.router.pop();
-              },
-              onChanged: (searchTerm) {
-                print('searchTerm changed to $searchTerm');
-              },
+            Consumer(
+              builder: ((context, ref, child) {
+                return SearchAppBar(
+                  onCancel: () {
+                    context.router.pop();
+                  },
+                  onChanged: (searchTerm) => ref
+                      .watch(searchInputProvider.notifier)
+                      .state = searchTerm,
+                );
+              }),
             ),
+            const SearchBody()
           ],
         ),
       ),
